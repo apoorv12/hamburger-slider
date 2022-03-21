feadc9bff7987bcad6836a5d4d5d4d2e3c83ac7b
Apoorv khandelwal <apoorvkhandelwal86@gmail.com>
2022-03-21 22:24:59 +0530

    Project completed with transtioning hamburger and navbar

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const burger = document.querySelector('.burger');
 const navbar = document.querySelector('.navlist');
 
-burger.addEventListener('click', (e)=> {
-    navbar.classList.toggle('display')
+// burger.addEventListener('click', (e)=> {
+//     navbar.classList.toggle('display')
+// });
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+    burger.classList.toggle('open');
 });
 
 
